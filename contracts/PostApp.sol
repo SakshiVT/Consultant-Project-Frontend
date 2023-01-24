848ab80679defbf5999f08a9e536a8d506fd0c18
VinVash <2020kucp1046@iiitkota.ac.in>
2023-01-25 02:17:44 +0530

    added the automation functionality from chainlink

--- contracts/PostApp.sol
+++ contracts/PostApp.sol
@@ -1,25 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-contract SocialMedia {
+import "@chainlink/contracts/src/v0.8/AutomationCompatible.sol";
+
+error SocialMedia__UpkeepNotNeeded();
+
+contract SocialMedia is AutomationCompatibleInterface {
     struct Post {
         uint256 id;
         address author;
         string content;
         uint256 timestamp;
-        // mapping (address => bool) likes;
-        // mapping (address => string) comments;
         bool isCensored;
         string imageURL;
     }
 
-    mapping(uint256 => mapping(address => uint256)) public likes; // change to bool
+    // mapping(uint256 => mapping(address => uint256)) public likes; // change to bool
+    mapping(uint256 => mapping(address => bool)) public likes;
+    mapping(uint256 => mapping(address => bool)) public hasComments; // whether a postId has been liked by an address.
     mapping(uint256 => mapping(address => string)) public comments;
 
+    address[3] public signers; // this array to be updated with the sign-up / sign-in feature.
+    address[3] public leaderboard; // this array to be updated by sorting the users by their scores.
+
     uint256 public numPosts; // number of posts on the deso.
-    mapping(uint256 => Post) public posts; // storse all posts on the deso.
+    mapping(uint256 => Post) public posts; // stores all posts on the deso.
+
     mapping(address => Post[]) public userPosts; // stores all posts on the deso by a particular user.
     mapping(address => uint256) public postCount; // stores the no. of posts on the deso by a particular user.
+
+    uint256 public immutable interval; // time after which the leaderboard needs to be reset.
+    uint256 public lastTimestamp; // the last timestamp at which the leaderboard was reset.
+
     mapping(address => bool) public isModerator; // stores whether a particular address is a moderator or not.
     address public owner; // stores the address of the owner of this contract.
     uint256 idCount;
@@ -30,8 +42,19 @@ contract SocialMedia {
     event CensoredPost(address bostAuthor, uint256 postld);
     event UncensoredPost(address postAuthor, uint256 postld);
 
-    constructor() {
+    constructor(
+        uint256 _interval,
+        address _ad1,
+        address _ad2,
+        address _ad3
+    ) {
         owner = msg.sender;
+        interval = _interval;
+        lastTimestamp = block.timestamp;
+
+        signers[0] = _ad1;
+        signers[1] = _ad2;
+        signers[2] = _ad3;
     }
 
     function createPost(string memory _content, string memory _imageURL)
@@ -46,14 +69,60 @@ contract SocialMedia {
             _imageURL
         );
 
+        posts[numPosts] = post; // adding to the total no. of posts.
         userPosts[msg.sender].push(post);
 
-        //setting postCount mapping
+        // setting postCount mapping
         numPosts++;
         postCount[msg.sender]++;
         idCount++;
 
         emit NewPost(msg.sender, _content, block.timestamp);
+    }
+
+    function checkUpkeep(
+        bytes memory /* checkData */
+    )
+        public
+        view
+        override
+        returns (
+            bool upkeepNeeded,
+            bytes memory /* performData */
+        )
+    {
+        bool timePassed = ((block.timestamp - lastTimestamp) > interval);
+
+        upkeepNeeded = timePassed;
+    }
+
+    function performUpkeep(
+        bytes calldata /* performData */
+    ) external override {
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if (!upkeepNeeded) {
+            revert SocialMedia__UpkeepNotNeeded();
+        }
+
+        // Sort the users and update the leaderboard, here hard coding the values.
+        leaderboard[0] = signers[1];
+        leaderboard[1] = signers[2];
+        leaderboard[2] = signers[0];
+    }
+
+    // Gets score of a particularuseraddress.
+    function getScore(address _address) internal view returns (uint256 score) {
+        for (uint256 i = 0; i < numPosts; i++) {
+            if (likes[i][_address]) {
+                score += 5;
+            }
+
+            if (hasComments[i][_address]) {
+                score += 10;
+            }
+        }
+
+        score += (userPosts[_address].length * 10);
     }
 
     // function likePost(address postAuthor, uint postld) public {
